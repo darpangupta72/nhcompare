60ef71c0fed36c5e8b82c505171e9bb695b28422
Pranjal Kumar <Pranjal Kumar>
2017-03-11 00:00:51 +0530

    HTML file to test login

---
+++ admin_welcome_html.php
@@ -0,0 +1,38 @@
+<?php
+    require_once 'test.php';
+?>
+
+<html>
+    <head>
+        <title>ADMIN TOGGLE MENU</title>
+    
+        <style>
+            li {list-style: none;}
+            body{
+                background-color: #FFFFFF;
+                background: url(123.jpg) no-repeat center;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <center>
+            <h2> <font color=#000000>Welcome ADMINISTRATOR</font></h2>
+        </center>
+
+        <div style=" position: relative; 
+            top: 20%;">
+            <ul>
+            <center>
+            <form method="POST">
+                <li>Username:<input type="text" name="Username" /></li>
+                <li>Password:  <input type="password" name="Password" /></li>
+                <br>
+                <li>&emsp;&emsp;&emsp;&emsp;<button name="command" value="login">LOGIN</button> &emsp; <button name="command" value="register">SIGN UP</button> </li>
+            </form>
+            </center>
+            </ul>
+        </div>
+    </body>
+
+</html>
