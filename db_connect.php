07dda705850117cec4bb3f02dc2318010473e508
darpangupta72 <darpangupta72@gmail.com>
2017-03-11 11:52:37 +0530

    updated db_connect to automatically connect to both systems

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,19 @@
 <?php
 
-// database configuration variables
+$u_agent = $_SERVER['HTTP_USER_AGENT']; 
 
+// database configuration variables
 $host = "host = localhost";
 $port = "port = 5432";
-$dbname = "dbname = nhcompare";
-$credentials = "user = postgres password = Dddddd1.";
- 
+            
+// First get the platform
+// For Darpan, Pranjal
+if (preg_match('/linux/i', $u_agent)) {
+	$dbname = "dbname = nhcompare";
+	$credentials = "user = postgres password = Dddddd1.";
+} elseif (preg_match('/windows|win32/i', $u_agent)) {
+	$dbname = "dbname = postgres";
+	$credentials = "user = postgres password = mihir.k";
+}
+
 ?>
