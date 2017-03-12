38f1445c65f74bac9a1bc5bee60934e436a9682c
darpangupta72 <darpangupta72@gmail.com>
2017-03-12 09:54:55 +0530

    implementing search using search_nh.php

--- search_nh_html.php
+++ search_nh_html.php
@@ -67,31 +67,19 @@
 if(isset($_POST['command'])) {
 
     switch ($_POST['command']) {
+
         case 'logout':
+
             session_unset();
             session_destroy();
             header("Location: login_html.php");
             break;
         
         case 'submit':
-            require_once 'db_connect.php';
-            $conn = pg_connect("$host $port $dbname $credentials");
-            $zip=$_POST['zip'];
-            $sql = "SELECT provnum, provname, state FROM provider_info WHERE state = '$zip'";
-            $result = pg_query($conn,$sql);
 
-            echo "<center><table border='1'><tr><th>Provider No.</th><th>Provider Name</th><th>Zip Code</th></tr>";
-
-            while($row = pg_fetch_assoc($result)) {
-                echo "<tr>";
-                echo "<td>".$row['provnum']."</td>";
-                echo "<td>".$row['provname']."</td>";
-                echo "<td>".$row['state']."</td>";
-                echo "</tr>";    
-            }
-
-            echo "</table></center>";
+            require_once 'search_nh.php';
             break;
+        
         default:
             # code...
             break;
